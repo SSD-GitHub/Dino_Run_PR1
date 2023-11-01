a0612fd78b4e9d3d8f5b5f9c86cf33ddca36842f
SSD-GitHub <22308693@bucks.ac.uk>
2023-11-01 19:58:51 +0000

    Improved Code Readability & Added Finishing Touches
    Added more comments to the code to enhance readability and added some finishing touches.

--- Dino Run.py
+++ Dino Run.py
@@ -1,14 +1,16 @@
-#install 'python -m pip install -U pygame --user' before attempting to run this game.
+#install 'python -m pip install -U pygame --user' before attempting to run this game on new computers.
 
-#importing the necessary libraries and initialising them.
+#Game Initialisation
+#importing the necessary libraries.
 import pygame
-pygame.init()
 import random
+pygame.init() #initialising the pygame library.
 
-#Establishing global constants
+#Establishing global Constants
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #creates screen surface to allow the game to be displayed.
+pygame.display.set_caption('Dino Run') #Sets window name to Dino Run
 
 #Character Sprite State Constants
 RUNNING = [pygame.image.load("Assets\\Dino\\DinoRun1.png"), 
@@ -33,7 +35,7 @@ CLOUD = pygame.image.load("Assets\\Other\\Cloud.png")
 
 BACKGROUND = pygame.image.load("Assets\\Other\\Track.png")
 
-#Creating classes
+#Creating the Dinosaur class
 class Dinosaur:
     x = 80
     y = 310
@@ -51,13 +53,13 @@ class Dinosaur:
         self.dino_jump = False
 
         self.step_index = 0 #to help animate the dinosaur
-        self.jump_vel = self.JUMP_VEL
+        self.jump_vel = self.JUMP_VEL #to ensure that the dinosaur jumps properly.
         self.image = self.run_img[0] #to set the dinosaur's inital sprite state.
         self.dino_rect = self.image.get_rect() #establishes the player's hitbox to allow for the implementation of object collision detection and handling.
         self.dino_rect.x = self.x #aligns the hitbox with the dinosaur's x coordinate.
         self.dino_rect.y = self.y #aligns the hitbox with the dinosaur's y coordinate.
 
-
+    #ensures the Dinosaur remains up to date with the player inputs.
     def update(self, userInput):
         if self.dino_duck: #ensures that the Dinosaur ducks when it is supposed to.
             self.duck()
@@ -83,6 +85,7 @@ class Dinosaur:
             self.dino_run = True
             self.dino_jump = False
 
+    #Makes the dinosaur duck
     def duck(self):
         if self.step_index // 5 < len(self.duck_img):
             self.image = self.duck_img[self.step_index // 5]
@@ -92,7 +95,7 @@ class Dinosaur:
         self.dino_rect.x = self.x
         self.dino_rect.y = self.y_duck
         
-
+    #makes the Dinosaur run
     def run(self):
         if self.step_index // 5 < len(self.run_img):
             self.image = self.run_img[self.step_index // 5]
@@ -102,7 +105,7 @@ class Dinosaur:
         self.dino_rect.x = self.x
         self.dino_rect.y = self.y
        
-
+    #makes the Dinosaur jump
     def jump(self):
         self.image = self.jump_img
         if self.dino_jump:
@@ -112,9 +115,11 @@ class Dinosaur:
             self.dino_jump = False
             self.jump_vel = self.JUMP_VEL
 
+    #Draws the Dinosaur on the screen.
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
 
+#Defining the Cloud class
 class Cloud:
     def __init__(self):
         self.x = SCREEN_WIDTH + random.randint(800, 1000)
@@ -128,9 +133,11 @@ class Cloud:
             self.x = SCREEN_WIDTH + random.randint(2500, 3000)
             self.y = random.randint(50, 100)
 
+    #Draws obstacles on the screen.
     def draw(self, SCREEEN):
         SCREEN.blit(self.image, (self.x, self.y))
 
+#Creates the Obstacle class
 class Obstacle:
     def __init__(self, image, type):
          self.image = image
@@ -140,26 +147,25 @@ class Obstacle:
 
     def update(self):
         self.rect.x -= game_speed
-        if self.rect.x <-self.rect.width:
+        if self.rect.x <-self.rect.width: #Ensures that Obstacles are removed as they disappear off the screen.
              obstacles.pop()
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image[self.type], self.rect)
 
-class smallcactus(Obstacle):
+class smallcactus(Obstacle): #creates the small cactus class to allow for multiple cacti to be spawned.
      def __init__(self, image):
         self.type = random.randint(0,2)
         super().__init__(image, self.type)
         self.rect.y = 325
 
-
-class Largecactus(Obstacle):
+class Largecactus(Obstacle): #creates the large cactus class to allow for multiple cacti to be spawned.
      def __init__(self, image):
         self.type = random.randint(0,2)
         super().__init__(image, self.type)
         self.rect.y = 300
 
-
+#Creating the Teradactyl class (called Bird for simplicity sake)
 class Bird(Obstacle):
      def __init__(self, image):
         self.type = 0
@@ -173,53 +179,54 @@ class Bird(Obstacle):
         SCREEN.blit(self.image[self.index//5], self.rect)
         self.index += 1
 
-#Creating Game Loop
+#Creating the Game Loop
 def main():
-    global game_speed, x_bg, y_bg, points, obstacles
+    global game_speed, x_bg, y_bg, points, obstacles #makes the specified variables global in scope (they can be accessed throughout the program)
     run = True
     clock = pygame.time.Clock() #to maintain the pace of the game.
     player = Dinosaur() #Created player object from class Dinosaur
     cloud = Cloud() #Created an object of the cloud class
-    game_speed = 14
+    game_speed = 14 #sets game speed.
     x_bg = 0
     y_bg = 380
-    points = 0 
+    points = 0 #sets points to 0.
     font= pygame.font.Font('freesansbold.ttf',20)
     obstacles = [] #establishes an array for all of the obstacles
-    death_count = 0
+    death_count = 0 #stores the number of times the player dies.
 
     def score():
-        global points, game_speed
-        points += 1 
-        if points % 100 == 0: 
-            game_speed += 1
-            text = font. render ("points: "+ str(points), True, (0, 0 ,0))
+        global points, game_speed #makes points and game_speed global in scope.
+        points += 1 #increments points by 1.
+        if points % 100 == 0: #if points divided by 100 has 0 as a remainder then do the following code.
+            game_speed += 1 
+            text = font.render ("points: "+ points, True, (0, 0 ,0))
             textRect = text.get_rect ()
-            textRect.center = (1000, 40)
-            SCREEN.blit(text,textRect)                      
+            textRect.center = (1000, 40) #centers the text.
+            SCREEN.blit(text,textRect) #prints text to the screen.                     
                    
     def background():
         global x_bg, y_bg 
-        image_width = BG.get_width()
-        SCREEN.blit(BG, (x_bg, y_bg))
-        SCREEN.blit(BG, (image_width + x_bg, y_bg)), 
+        image_width = BACKGROUND.get_width() #Gets the width of the background image.
+        SCREEN.blit(BACKGROUND, (x_bg, y_bg)) #Displays the background
+        SCREEN.blit(BACKGROUND, (image_width + x_bg, y_bg)), 
         if x_bg <= -image_width:
-            SCREEN.blit(BG, (image_width + x_bg, y_bg))
+            SCREEN.blit(BACKGROUND, (image_width + x_bg, y_bg)) #makes the background image move.
             x_bg = 0
             x_bg -= game_speed
 
-    while run:
+    while run: #whilst the game is running.
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: #When close button pressed, close the program.
+            if event.key == pygame.K_ESCAPE: #When escape button pressed, close the program.
                 pygame.quit()
                 run = False
 
         SCREEN.fill((255, 255, 255)) #You have to pass in a tuple to represent RGB values in pygame.
         userInput = pygame.key.get_pressed() #stores the user input.
 
-        player.draw(SCREEN)
+        player.draw(SCREEN) #draws the player character (the dinosaur) to the screen.
         player.update(userInput)
 
+        #Ensures that obstacles spawn and get added to the obstacle list for easy management.
         if len(obstacles) == 0:
            if random.randint(0,2) == 0:
             obstacles.append(smallcactus(SMALL_CACTUS))
@@ -228,46 +235,46 @@ def main():
            elif random.randint(0,2) == 2:
                obstacles.append(Bird(TERADACTYL))
 
+        #Ensures obstacles get drawn to the screen
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
             obstacle.update()
-            if player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(2000)
-                death_count += 1
+            if player.dino_rect.colliderect(obstacle.rect): #detects collisions between the player and obstacles
+                pygame.time.delay(500) #pauses the game for 500 miliseconds.
+                death_count += 1 #increments the death counter variable by one. (+= 1 increments whichever variable it is assigned to by 1)
                 menu(death_count)
            
-
+        #Ensures that clouds get drawn to the screen.
         cloud.draw(SCREEN)
         cloud.update()
 
         clock.tick(30)
-        pygame.display.update()
+        pygame.display.update() #updates the display.
 
 def menu(death_count):
     global points
     run = True
     while run:
-        SCREEN.fill((255, 255, 255))
-        font = pygame.font.Font("freesansbold.ttf", 30)# what is freesansbold
+        SCREEN.fill((255, 255, 255)) #makes the game background white.
+        font = pygame.font.Font("freesansbold.ttf", 30) #sets the font for the game
  
-        if death_count == 0:
+        if death_count == 0: #if the game has just started, then display "press any key to start".
             text = font.render("Press any Key to Start", True, (0, 0, 0))
-        elif death_count > 0:
+        elif death_count > 0: #if the player has died before and the game hasn't just started, then display "press any key to restart".
             text = font.render("Press any key to Restart", True, (0, 0, 0))
-            score = font.render("Your Score: " + str(points), True, (0, 0, 0))
+            score = font.render("Your Score: " + str(points), True, (0, 0, 0)) #Prints your score to the screen after you die.
             scoreRect = score.get_rect()
-            scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
-            SCREEN.blit(score, scoreRect)
+            scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50) #centers your score.
+            SCREEN.blit(score, scoreRect) #prints your score to the screen.
         textRect = text.get_rect()
-        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-        SCREEN.blit(text, textRect)
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2) #centers the text.
+        SCREEN.blit(text, textRect) #outputs the text to the screen.
         SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN: #if any key pressed, start the game.
                 main()
  
- 
-menu(death_count=0)
+menu(death_count=0) #starts the game by initially calling the main function in the game loop.
